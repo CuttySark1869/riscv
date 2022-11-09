63a6167796610a23596c50536fe83a9c26900172
Yunjie Gu <yunjie.gu1@gmail.com>
2022-11-09 19:32:01 +0000

    two threads

--- applications/startup.c
+++ applications/startup.c
@@ -15,9 +15,34 @@
 #include <rtthread.h>
 #include "board.h"
 
+void thread1_entry(void* parameter)
+{
+	while(1)
+	{
+		rt_thread_yield();
+	}
+}
+
+void thread2_entry(void* parameter)
+{
+	while(1)
+	{
+		rt_thread_yield();
+	}
+}
+
 int main(void)
 {
     volatile unsigned long x=100000000;
+    rt_thread_t thread1=NULL,thread2=NULL;
+
+    rt_uint32_t para1=0,para2=0;
+
+    thread1 = rt_thread_create("thread1",thread1_entry,&para1,400,5,10);
+    thread2 = rt_thread_create("thread2",thread2_entry,&para2,400,5,10);
+
+    rt_thread_startup(thread1);
+    rt_thread_startup(thread2);
 
     while(x)
     {
@@ -26,5 +51,4 @@ int main(void)
 
     x = 100000000;
 }
-
 /*@}*/
